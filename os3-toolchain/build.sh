f71504d58ea148ddb48ea3781e052668383f14be
Stepan Usatiuk <stepan@usatiuk.com>
2024-03-29 17:56:25 +0100

    toolchain build.sh uncomment oops

--- os3-toolchain/build.sh
+++ os3-toolchain/build.sh
@@ -14,26 +14,26 @@ cd "$SCRIPT_DIR"
 
 mkdir -p "$OS2_ROOT/sysroot"
 
-# cd binutils-x86_64-os3
-# mkdir -p build
-# cd build
-# ../binutils-2.41/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot="$OS2_ROOT/sysroot" --disable-nls --disable-werror
-# make -j$(nproc)
-# make install
+cd binutils-x86_64-os3
+mkdir -p build
+cd build
+../binutils-2.41/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot="$OS2_ROOT/sysroot" --disable-nls --disable-werror
+make -j$(nproc)
+make install
 
-# cd ../../
+cd ../../
 
-# cd gcc-x86_64-os3
-# rm -rf build
-# mkdir -p build
-# cd build
-# ../gcc-13.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --with-newlib --without-headers --enable-version-specific-runtime-libs --with-gmp=/opt/homebrew --with-mpc=/opt/homebrew --with-mpfr=/opt/homebrew
-# make -j$(nproc) all-gcc
-# make -j$(nproc) all-target-libgcc
-# make install-gcc
-# make install-target-libgcc
+cd gcc-x86_64-os3
+rm -rf build
+mkdir -p build
+cd build
+../gcc-13.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --with-newlib --without-headers --enable-version-specific-runtime-libs --with-gmp=/opt/homebrew --with-mpc=/opt/homebrew --with-mpfr=/opt/homebrew
+make -j$(nproc) all-gcc
+make -j$(nproc) all-target-libgcc
+make install-gcc
+make install-target-libgcc
 
-# cd ../../
+cd ../../
 cd newlib
 mkdir build
 cd build
