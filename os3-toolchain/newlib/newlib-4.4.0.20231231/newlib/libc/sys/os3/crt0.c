35f67a70f5bff5b3aaa34a920f053a2f999676e8
Stepan Usatiuk <stepan@usatiuk.com>
2024-03-30 00:17:54 +0100

    Don't forget the constructors/destructors in crt0.c!

--- os3-toolchain/newlib/newlib-4.4.0.20231231/newlib/libc/sys/os3/crt0.c
+++ os3-toolchain/newlib/newlib-4.4.0.20231231/newlib/libc/sys/os3/crt0.c
@@ -2,8 +2,12 @@
  
 extern void exit(int code);
 extern int main ();
- 
+extern void __libc_init_array();
+extern void __libc_fini_array();
+
 void _start() {
+    __libc_init_array();
     int ex = main();
+    __libc_fini_array();
     exit(ex);
 }
