3ca114e0f6495b817805f6935089490e3f16b4e1
Stepan Usatiuk <stepan@usatiuk.com>
2024-03-30 13:05:15 +0100

    C++ exceptions test

---
+++ clionenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# FIXME:
+export PREFIX="/Users/stepus53/projects/os2/toolchain/gcc-x86_64-os3-prefix/"
+export TARGET=x86_64-os3
+export PATH="$PREFIX/bin:$PATH"
+
--- src/test/hello2.cpp
+++ src/test/hello2.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+void badfn() {
+    throw std::runtime_error("oops");
+}
+
 int main() {
-    std::cout << "Hi!" << std::endl;
+    std::cout << "hi" << std::endl;
+    try {
+        std::cout << "try" << std::endl;
+        badfn();
+        std::cout << "ok" << std::endl;
+    } catch (std::exception &e) {
+        std::cout << "huh" << std::endl;
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << "fin" << std::endl;
 }
