d81848b0bec10cdf883d1992219cbe2bf7656ee1
Stepan Usatiuk <stepan@usatiuk.com>
2024-04-26 19:22:34 +0200

    LimineFramebuffer: add virtual destructor

--- src/arch/x86/LimineFramebuffer.hpp
+++ src/arch/x86/LimineFramebuffer.hpp
@@ -15,14 +15,14 @@
 
 class LimineFramebuffer : public Framebuffer {
 public:
-    LimineFramebuffer(limine_framebuffer *limine_framebuffer);
+             LimineFramebuffer(limine_framebuffer *limine_framebuffer);
+    virtual ~LimineFramebuffer() = default;
 
     void set(size_t x, size_t y, uint32_t color) override;
     int  get(size_t x, size_t y) override;
 
 private:
-
-    uint32_t* coord_to_ptr(size_t x, size_t y);
+    uint32_t *coord_to_ptr(size_t x, size_t y);
 
     limine_framebuffer *_backing;
 };
